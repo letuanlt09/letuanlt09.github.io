7d95f6c99ab0788bcc6e54bc2045967e8db843fd
Tuan Le <letuanlt09@gmail.com>
2020-09-24 11:50:12 -0400

    improved

--- jquery.js
+++ jquery.js
@@ -1,4 +1,19 @@
-function testButton() {
+function testn() {
   console.log("here");
-  $("#title").hide();
+}
+function aboutClick() {
+  $("#projectContainer, #workContainer, #resumeContainer").addClass("hidden");
+  $("#aboutContainer").removeClass("hidden");
+}
+function projectClick() {
+  $("#aboutContainer, #workContainer, #resumeContainer").addClass("hidden");
+  $("#projectContainer").removeClass("hidden");
+}
+function workClick() {
+  $("#aboutContainer, #projectContainer, #resumeContainer").addClass("hidden");
+  $("#workContainer").removeClass("hidden");
+}
+function resumeClick() {
+  $("#aboutContainer, #projectContainer, #workContainer").addClass("hidden");
+  $("#resumeContainer").removeClass("hidden");
 }
